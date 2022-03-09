ae33bc7e4d0e5d5915a5c1735314105995df4017
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-03-09 11:26:12 -0300

    Requisito 6 productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,13 +28,24 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
+    expect(Array.isArray(productDetails('Alcool gel', 'Máscara'))).toBe(true);
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect.arrayContaining(productDetails([0, 1]));
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails()[0, 1] === 'object').toBe(true);
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    const xablau = Object.values(productDetails('xablau', 'xableu')[0]);
+    const xableu = Object.values(productDetails('xablau', 'xableu')[1]);
+    expect(xablau !== xableu).toBe(true);
     // Teste se os dois productIds terminam com 123.
+    // const funcao = productDetails('primeiro', 'segundo');
+    expect(productDetails()[0].details.productId.endsWith('123')).toBe(true);
+    expect(productDetails()[1].details.productId.endsWith('123')).toBe(true);
+    
   });
 });
