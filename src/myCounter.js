fac18c72ee59e2e345bce942b6fb96cfb5c0753a
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-03-09 14:44:32 -0300

    Requisito 8 myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,15 +16,16 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
-const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+const myCounter = () => { 
+  const myArray = []; // troquei por const
+  for (let counter = 0; counter <= 3; counter += 1) { // troquei por let
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) { // troquei por let
       myArray.push(counter);
     }
   }
   return myArray;
 };
+console.log(myCounter());
 
 module.exports = myCounter;
